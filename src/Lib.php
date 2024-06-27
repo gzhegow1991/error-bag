035ae94c8fec8a0e0a5e5113ff001894140746b0
gzhegow <6562680@gmail.com>
2024-06-27 18:57:04 +0300

    main

--- src/Lib.php
+++ src/Lib.php
@@ -191,7 +191,10 @@ class Lib
         $result[ 'messageDataList' ] = $messageDataList;
 
         $messageDataList = $messageDataList ?? [];
-        $messageData = array_replace(...$messageDataList);
+        $messageData = $messageDataList
+            ? array_replace(...$messageDataList)
+            : [];
+
         $messageObject = (object) ([ $message ] + $messageData);
 
         $result[ 'message' ] = $message;
